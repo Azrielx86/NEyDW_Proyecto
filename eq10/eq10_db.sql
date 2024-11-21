c90078e62f33a63465912106c3928e13e9cfe016
Azrielx86 <edgar1569@outlook.com>
2024-11-20 19:39:39 -0600

    updated database

--- eq10/eq10_db.sql
+++ eq10/eq10_db.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS producto
 (
     id          INT           NOT NULL AUTO_INCREMENT,
     nombre      VARCHAR(128)  NOT NULL,
-    precio      DECIMAL       NOT NULL,
+    precio      DECIMAL(10,2)       NOT NULL,
     descripcion VARCHAR(1024) NOT NULL,
     img_path    VARCHAR(512)  NOT NULL,
     id_pais     INT           NOT NULL,
@@ -88,6 +88,7 @@ CREATE TABLE IF NOT EXISTS compra_cliente
 
 
 /* ================================================= INSERTS ======================================================== */
+/*
 INSERT INTO pais (nombre)
 VALUES ('China');
 INSERT INTO pais (nombre)
@@ -273,3 +274,214 @@ INSERT INTO producto_categoria (id_producto, id_categoria)
 VALUES (19, 2);
 INSERT INTO producto_categoria (id_producto, id_categoria)
 VALUES (20, 1);
+*/
+
+INSERT INTO pais (nombre)
+VALUES ('China');
+INSERT INTO pais (nombre)
+VALUES ('USA');
+INSERT INTO pais (nombre)
+VALUES ('Japón');
+INSERT INTO pais (nombre)
+VALUES ('Corea del sur');
+
+INSERT INTO categoria (id, nombre)
+VALUES (1, 'Snack');
+INSERT INTO categoria (id, nombre)
+VALUES (2, 'Popcorn');
+INSERT INTO categoria (id, nombre)
+VALUES (3, 'Cookie');
+INSERT INTO categoria (id, nombre)
+VALUES (4, 'Soda');
+INSERT INTO categoria (nombre)
+VALUES ('Chocolate');
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (1, 'Cheetos Crunchy', 5.99, 'Cheese 8.5 oz', 'cheetosCrunchy.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (1, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (2, 'Cheetos Flamin'' Hot Crunchy', 5.99, 'Flamin'' Hot 8.5 oz', 'cheetosFlaminHot.png', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (2, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (3, 'Cheetos Flamin'' Hot Limon Crunchy', 5.99, 'Flamin'' Hot Limon 8.5 oz', 'cheetosFlaminHotLimon.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (3, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (4, 'Cheetos Xxtra Flamin'' Hot Crunchy', 5.99, 'Xxtra Flamin'' Hot 8.5 oz', 'cheetosXxtraFlaminHot.png', 3);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (4, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (5, 'Cheetos Cheddar JalapeÃ±o Crunchy', 5.99, 'JalapeÃ±o 8.5 oz', 'cheetosJalapeno.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (5, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (6, 'Cheetos Mexican Street Corn', 5.99, 'Mexican Street Corn 8.5 oz', 'cheetosMexican.png', 3);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (6, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (7, 'Doritos Spicy Sweet Chili', 6.99, 'Spicy Sweet Chili 8.5 oz', 'doritosSpicySweetChili.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (7, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (8, 'Doritos Cool Ranch', 6.99, 'Ranch 8.5 oz', 'doritosCoolRanch.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (8, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (9, 'Doritos Spicy Nacho', 6.99, 'Spicy Nacho 8.5 oz', 'doritosSpicyNacho.png', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (9, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (10, 'Doritos Flamin'' Hot Nacho', 6.99, 'Flamin'' Hot Nacho 8.5 oz', 'doritosFlaminHot.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (10, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (11, 'Doritos Flamin'' Hot Limon', 6.99, 'Flamin'' Hot Limon 8.5 oz', 'doritosFlaminHotLimon.png', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (11, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (12, 'Doritos Sweet & Tangy BBQ', 6.99, 'Sweet & Tangy BBQ 8.5 oz', 'doritosBBQ.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (12, 1);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (13, 'Kernel Season''s Butter Popcorn Seasoning', 4.99, 'Butter Popcorn 2.85 oz', 'kernelSeasonsButter.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (13, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (13, 2);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (14, 'Kernel Season''s Caramel Corn Popcorn Seasoning', 4.99, 'Caramel Corn Popcorn 2.85 oz',
+        'kernelSeasonsCheesyCaramelCorn.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (14, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (14, 2);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (15, 'Kernel Season''s Cheesy JalapeÃ±o Popcorn Seasoning', 4.99, 'Cheesy JalapeÃ±o Popcorn 2.85 oz',
+        'kernelSeasonsCheesyJalapeno.png', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (15, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (15, 2);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (16, 'Kernel Season''s Chile Limon Popcorn Seasoning', 4.99, 'Chile Limon Popcorn 2.85 oz',
+        'kernelSeasonsChileLimon.png', 3);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (16, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (16, 2);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (17, 'Kernel Season''s Nacho Cheddar Popcorn Seasoning', 4.99, 'Nacho Cheddar Popcorn 2.85 oz',
+        'kernelSeasonsNachoCheddar.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (17, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (17, 2);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (18, 'Kernel Season''s Ranch Popcorn Seasoning', 4.99, 'Ranch Popcorn 2.85 oz', 'kernelSeasonsRanch.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (18, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (18, 2);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (19, 'Teddy Grahams Chocolatey Chips', 20.99, 'Chocolate 10 oz', 'teddyGrahamsChocolatey.jpg', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (19, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (19, 3);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (20, 'Teddy Grahams Cinnamon', 20.99, 'Cinnamon 10 oz', 'teddyGrahamsCinnamon.jpg', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (20, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (20, 3);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (21, 'Teddy Grahams Cocoa', 20.99, 'Cocoa 10 oz', 'teddyGrahamsCocoa.jpg', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (21, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (21, 3);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (22, 'Teddy Grahams Honey', 20.99, 'Honey 10 oz', 'teddyGrahamsHoney.jpg', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (22, 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (22, 3);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (23, 'Caffeine-Free Coca-Cola', 1.49, 'Caffeine Free 12 oz', 'cocaColaCaffeineFree.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (23, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (24, 'Caffeine-Free Diet Coca-Cola', 1.49, 'Caffeine Free Diet 12 oz', 'cocaColaCaffeineFreeDiet.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (24, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (25, 'Coca-Cola Cherry', 1.49, 'Cherry 12 oz', 'cocaColaCherry.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (25, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (26, 'Coca-Cola Zero Cherry', 1.49, 'Zero Cherry 12 oz', 'cocaColaCherryZero.png', 3);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (26, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (27, 'Coca-Cola Vanilla', 1.49, 'Vanilla 12 oz', 'cocaColaVanilla.png', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (27, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (28, 'Coca-Cola Vanilla Zero', 1.49, 'Vanilla Zero 12 oz', 'cocaColaCherryZero.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (28, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (29, 'Fanta', 2.99, 'Orange 20 oz', 'fanta.png', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (29, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (30, 'Fanta Zero', 2.99, 'Orange Zero 20 oz', 'fantaZero.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (30, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (31, 'Fanta Grape', 2.99, 'Grape 20 oz', 'fantaGrape.png', 1);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (31, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (32, 'Fanta Pineapple', 2.99, 'Pineapple 20 oz', 'fantaPineapple.png', 2);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (32, 4);
+
+INSERT INTO producto (id, nombre, precio, descripcion, img_path, id_pais)
+VALUES (33, 'Fanta Strawberry', 2.99, 'Strawberry 20 oz', 'fantaStrawberry.png', 4);
+INSERT INTO producto_categoria (id_producto, id_categoria)
+VALUES (33, 4);
+
